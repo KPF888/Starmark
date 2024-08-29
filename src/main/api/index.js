3444503f7752e922f50b3d7bd00881f731178565
Comekeybord <qclbyx@qq.com>
2024-08-29 09:39:02 +0800

    delete: 删除冗余代码

--- src/main/api/index.js
+++ src/main/api/index.js
@@ -1,10 +1,8 @@
-import { bruteSubDomain } from './subdomain';
-import { ipcMain } from 'electron';
 import SubDomainListWindow from '../event/SubDomainListWindow.js';
+
 const listWindow = new SubDomainListWindow();
 
 /* 注册所有业务api */
 export const regAllApi = () => {
-  ipcMain.handle('bruteSubDomain', bruteSubDomain);
   listWindow.registerEvents();
 };
--- src/main/api/subdomain/index.js
+++
@@ -1,8 +0,0 @@
-import { SubDomainBrute } from '../../service/SubDomainBrute';
-
-export const bruteSubDomain = (event, options) => {
-  const { domainList, concurrent, timeout } = options;
-  console.log('bruteSubDomain: ', concurrent);
-  const brute = new SubDomainBrute(domainList, concurrent);
-  return brute.bruteForceSubdomains(event, timeout);
-};
