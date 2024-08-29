5d2aa3ee356b5982441dcda6d4aa58332e25fae3
Comekeybord <qclbyx@qq.com>
2024-08-29 11:27:21 +0800

    删库

--- src/main/api/index.js
+++
@@ -1,8 +0,0 @@
-import SubDomainListWindow from '../event/SubDomainListWindow.js';
-
-const listWindow = new SubDomainListWindow();
-
-/* 注册所有业务api */
-export const regAllApi = () => {
-  listWindow.registerEvents();
-};
--- src/renderer/src/page/urlutils/index.jsx
+++
@@ -1,9 +0,0 @@
-const UrlUtils = () => {
-  return (
-    <>
-      <h1>urlUtils page</h1>
-    </>
-  );
-};
-
-export default UrlUtils;
